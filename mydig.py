54fd0e2ed0b568bd9623d3a6c8d27e01f3b2d48f
Allen Kim <allenkim95@gmail.com>
2018-02-09 18:25:54 -0500

    Finalize part A

--- mydig.py
+++ mydig.py
@@ -6,7 +6,8 @@ from time import localtime, strftime
 from timeit import default_timer as timer
 
 # Function to contact root server, then top level domain, then name server
-def query_server(domain, typ, trace=False):
+def query_server(domain, typ, trace=False, simple=False):
+    start_time = timer()
     # intial root servers to query
     root_servers = (
     '198.41.0.4', # a.root-servers.net
@@ -27,7 +28,7 @@ def query_server(domain, typ, trace=False):
     servers = root_servers
     total_time = 0
     output = ""
-    if not trace:
+    if not trace and not simple:
         output += "QUESTION SECTION:\n"
         output += query.question[0].to_text()
         output += "\n\nANSWER SECTION:\n"
@@ -35,60 +36,69 @@ def query_server(domain, typ, trace=False):
         resp = None
         for server in servers:
             try:
-                start_time = timer()
                 resp = dns.query.udp(query, server, timeout=2.0)
-                end_time = timer()
-                total_time += end_time - start_time
                 break
             except:
                 continue
         if not resp:
-            print("Something crazy happened! All servers are down!")
-            return
+            return "Something crazy happened! All servers are down!"
         if resp.answer:
-            output += resp.answer[0].to_text() + '\n'
+            if not simple:
+                output += resp.answer[0].to_text() + '\n'
             if resp.answer[0].rdtype == dns.rdatatype.CNAME:
                 domain = resp.answer[0][0].target
                 query = dns.message.make_query(domain, typ)
                 servers = root_servers
                 continue
-            if not trace:
+            end_time = timer()
+            total_time += end_time - start_time
+            if not trace and not simple:
                 output += '\n'
                 output += "Query time: {} msec\n".format(int(total_time*1000))
                 output += "WHEN: {}\n".format(strftime("%a %b %-d %H:%M:%S %Y"))
                 output += "MSG SIZE rcvd: {}".format(len(resp.to_wire()))
-            print(output)
-            return
-        if resp.additional:
-            if trace:
-                output += resp.authority[0].to_text() + '\n'
-            rrset = resp.additional
+            if simple:
+                output += resp.answer[0][0].address
+            return output
+        if resp.authority:
             ns_servers = []
-            for rr in rrset:
-                if rr.rdtype == dns.rdatatype.A:
-                    ns_servers.append(rr[0].address)
-            servers = ns_servers
-        else:
-            if not trace:
-                output += "Query time: {} msec\n".format(total_time)
-                output += "WHEN: {}\n".format(strftime("%a %b %-d %H:%M:%S %Y"))
-                output += "MSG SIZE rcvd: {}".format(len(resp.to_wire()))
-            print(output)
-            return
+            if resp.additional:
+                if trace:
+                    output += resp.authority[0].to_text() + '\n'
+                rrset = resp.additional
+                for rr in rrset:
+                    if rr.rdtype == dns.rdatatype.A:
+                        ns_servers.append(rr[0].address)
+            else:
+                rauth = resp.authority[0]
+                for auth in rauth:
+                    if auth.rdtype == dns.rdatatype.NS:
+                        ns_servers.append(query_server(auth.to_text(),'A',simple=True))
+            if ns_servers:
+                servers = ns_servers
+                continue
+        end_time = timer()
+        total_time += end_time - start_time
+        if not trace and not simple:
+            output += "\n"
+            output += "Query time: {} msec\n".format(int(total_time*1000))
+            output += "WHEN: {}\n".format(strftime("%a %b %-d %H:%M:%S %Y"))
+            output += "MSG SIZE rcvd: {}".format(len(resp.to_wire()))
+        return output
 
 def main():
     parser = argparse.ArgumentParser(description="Get information out of DNS")
     parser.add_argument('name', metavar='name', type=str,
             help='can be any domain name')
     parser.add_argument('type', metavar='type', type=str, nargs='?', default='A',
-            help='defaults to A - supports A, MX, NS')
+            help='defaults to A')
     parser.add_argument('-D', dest='DNSSEC', action='store_true',
             help='enable DNSSEC in query')
     parser.add_argument('-T', dest='trace', action='store_true',
             help='trace name from the root down')
     args = parser.parse_args()
-    query_server(args.name,args.type,args.trace)
-
+    info = query_server(args.name,args.type,args.trace)
+    print(info)
 
 if __name__ == "__main__":
     main()
